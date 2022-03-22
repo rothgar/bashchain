a2c58c75306fe5a082452b0e9f09b6bc0f53d6fb
Pim Beune <git@beune.dev>
2022-03-22 17:45:16 +0100

    Update for shellcheck compliance
    This commit updates the two shell scripts so that they are shellcheck compliant. Shellcheck is a static code analyzer.

--- bashchain.sh
+++ bashchain.sh
@@ -3,16 +3,16 @@
 # how many leading zeros should we match
 LEADING_ZEROS=${1:-3}
 # generate a match string (eg 000)
-NEEDED_ZEROS=$(printf -- '0%.0s' $(seq ${LEADING_ZEROS})})
+NEEDED_ZEROS=$(printf -- '0%.0s' $(seq "${LEADING_ZEROS}"))
 # generate an initial uuid
 NEXT_HASH=$(uuidgen)
 # initialize counter for number of hashes
 COUNT=1
 
 # find the latest block file
-LATEST_BLOCK_FILE=$(ls -1v block_* | tail -1)
+LATEST_BLOCK_FILE=$(find . -type f -iname "block_*" | sort -n | cut -c 3- | tail -1)
 # save hash of the latest block
-LATEST_BLOCK_HASH=$(md5sum $LATEST_BLOCK_FILE | awk '{print $1}')
+LATEST_BLOCK_HASH=$(md5sum "$LATEST_BLOCK_FILE" | awk '{print $1}')
 # get the number for the next block file
 NEXT_BLOCK_NUMBER=${LATEST_BLOCK_FILE%%.*}
 NEXT_BLOCK_NUMBER=$((${NEXT_BLOCK_NUMBER#*_}+1))
@@ -27,7 +27,7 @@ until [[ "${NEXT_HASH:0:$LEADING_ZEROS}" == "${NEEDED_ZEROS}" ]]; do
 	# generate new seed each iteration
 	SEED=$(uuidgen)
 	# concat seed with last block hash and save new hash
-	NEXT_HASH=$(printf "${SEED}${LATEST_BLOCK_HASH}" | md5sum | awk '{print $1}')
+	NEXT_HASH=$(printf '%s%s' "${SEED}" "${LATEST_BLOCK_HASH}" | md5sum | awk '{print $1}')
 	# print seed and hash
 	echo "${SEED} : ${NEXT_HASH}"
 	# increment hash
